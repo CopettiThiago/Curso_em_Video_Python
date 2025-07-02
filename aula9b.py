27f2b86216216eef493917ff6e3964c74e73d8d8
Thiago Copetti <thiago.rc2009@hotmail.com>
2025-07-02 14:39:01 -0300

    aula

---
+++ aula9b.py
@@ -0,0 +1,19 @@
+frase = 'Curso em Vídeo Python'
+
+print(frase[3:13:2]) #fatiamento com passo
+print(frase[::2]) # salto 2 sem saber o inicio e o fim
+
+# para um texto com várias linhas pode-se usar o print com ''' no inicio e final
+
+print(frase.count('o')) # conta quantos o minusculos tem na frase
+print(frase.upper().count('O')) # upper coloca tudo em maiusculo e conta o total de 'O'
+print(len(frase.strip())) # strip remove os espaços e len conta os caracteres
+print(frase.replace('Python', 'Android')) # para substituir na frase, frase deve = a string
+print('Curso' in frase) # mostra se existe a palavra Curso na string
+print(frase.lower().find('vídeo')) # lower transforma toda a string em minuscula, find mostra a posição
+
+print(frase.split())
+
+dividido = frase.split()
+
+print(dividido[2][3]) # split divide a frase, nesse caso o dividido mostra a terceira palavra e a quarta letra
---
+++ desafio22.py
@@ -0,0 +1,6 @@
+nome = input('Informe seu nome completo :')
+
+print(nome.upper())
+print(nome.lower())
+print(len(nome.strip()))
+print(len(nome.split[0]))
